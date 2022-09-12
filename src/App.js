141c9422703d8f26ba195f57b4d5054a149e6ce4
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2022-09-12 19:47:50 +0530

    Added Subscribers  using Static method

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ function App() {
                     <span className="grid-item name-heading">Name</span>
                     <span className="grid-item phone-heading">Phone</span>
                 </div>
+                <div className="grid-container ">
+                    <span className="grid-item ">Shilpa</span>
+                    <span className="grid-item ">8888888888</span>
+                </div>
+                <div className="grid-container ">
+                    <span className="grid-item ">Srishti</span>
+                    <span className="grid-item ">9999999999</span>
+                </div>
+
+
+
             </div>
         </div>
     );
