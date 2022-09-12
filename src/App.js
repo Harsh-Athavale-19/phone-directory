803a1a8affddfa0dd2089be83f7f80dc66a0be10
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2022-09-12 19:16:50 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
-import './App.css';
-
 function App() {
   return (
     <div>
+    <div>Phone Directory</div>
+    <button>Add</button>
+    <div>
+      <span>Name</span><br />
+      <span>Phone</span>
+    </div>
       
     </div>
   );
