5f0ea3bdd794eef0ed3a507723b734c784d35f41
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2022-09-12 19:50:55 +0530

    Changed App To Class Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from "react";
-
-import Header from "./Header";
+import React, { Component } from "react";
+import Header from "./Header.js";
 import "./App.css";
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <div className="component-body-container">
-                <button className="custom-btn add-btn">Add</button>
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="component-body-container">
+                    <button className="custom-btn add-btn">Add</button>
 
-                <div className="grid-container heading-container">
-                    <span className="grid-item name-heading">Name</span>
-                    <span className="grid-item phone-heading">Phone</span>
+                    <div className="grid-container heading-container">
+                        <span className="grid-item name-heading">Name</span>
+                        <span className="grid-item phone-heading">Phone</span>
+                    </div>
+                    <div className="grid-container ">
+                        <span className="grid-item ">Shilpa</span>
+                        <span className="grid-item ">8888888888</span>
+                    </div>
+                    <div className="grid-container ">
+                        <span className="grid-item ">Srishti</span>
+                        <span className="grid-item ">9999999999</span>
+                    </div>
                 </div>
-                <div className="grid-container ">
-                    <span className="grid-item ">Shilpa</span>
-                    <span className="grid-item ">8888888888</span>
-                </div>
-                <div className="grid-container ">
-                    <span className="grid-item ">Srishti</span>
-                    <span className="grid-item ">9999999999</span>
-                </div>
-
-
-
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
