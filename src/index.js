6c4848d4800682a3f36ebf5140143b800097e72b
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2022-09-14 11:48:08 +0530

    Fixed Index.js Error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './common/common.css'
@@ -7,12 +7,15 @@ import PhoneDirectory from './PhoneDirectory';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <PhoneDirectory />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <PhoneDirectory />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
+// registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
